5329ae0b8c55fe2c3cfc0c348a4c4d25c0a99e28
jinnyjinnyjinjin <kkam2ya@gmail.com>
2022-11-08 12:15:05 +0900

    first commit

---
+++ src/main/java/com/jinnyjinnyjinjin/chart/StockHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinnyjinnyjinjin/chart/StockHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
