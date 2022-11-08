f63a2ff365c5b5c19655e67839ec6300c3efd7e9
jinnyjinnyjinjin <kkam2ya@gmail.com>
2022-11-08 22:50:51 +0900

    Delete Contructor

--- src/main/java/com/jinnyjinnyjinjin/chart/common/ApiResponse.java
+++ src/main/java/com/jinnyjinnyjinjin/chart/common/ApiResponse.java
@@ -2,24 +2,18 @@ package com.jinnyjinnyjinjin.chart.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
 public class ApiResponse {
 
-    private boolean success = true;
+    private boolean success;
     private String message;
-    private HttpStatus status;
     private Object data;
 
     public ApiResponse(boolean success, String message) {
         this.success = success;
         this.message = message;
-    }
-
-    public ApiResponse(Object data) {
-        this.data = data;
     }
 
     public ApiResponse(boolean success, Object data) {
